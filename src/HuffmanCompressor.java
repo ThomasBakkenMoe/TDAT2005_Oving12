fb9f68385437edcc529a9466f396da14cd813288
ThomasBakkenMoe <thomasbakkenmoe@gmail.com>
2019-11-14 11:26:14 +0100

    Created basic methods and structure

--- src/HuffmanCompressor.java
+++ src/HuffmanCompressor.java
@@ -1,6 +1,47 @@
 public class HuffmanCompressor {
 
+
+    private class Node{
+
+        char character;
+        int frequency;
+        Node left = null;
+        Node right = null;
+
+        Node(char character, int frequency){
+
+            this.character = character;
+            this.frequency = frequency;
+
+        }
+
+        void setLeft(Node left){
+            this.left = left;
+        }
+
+        void setRight(Node right){
+            this.right = right;
+        }
+
+        Node getLeft(){
+            return left;
+        }
+
+        Node getRight(){
+            return right;
+        }
+    }
+
+    private void findFrequency(String file){
+
+    }
+
+    public void compress(String file){
+
+    }
+
     public static void main(String[] args) {
-        
+
     }
 }
+
