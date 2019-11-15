031ee1d783eea82f50b00dd11b92c597b4bb027b
ThomasBakkenMoe <thomasbakkenmoe@gmail.com>
2019-11-15 14:06:13 +0100

    Finished compress method and export to file

--- src/HuffmanCompressor.java
+++ src/HuffmanCompressor.java
@@ -1,13 +1,9 @@
-import java.io.BufferedInputStream;
-import java.io.DataInputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.BitSet;
 import java.util.Collections;
 
 public class HuffmanCompressor {
-
 
     private class Node implements Comparable<Node> {
 
@@ -101,26 +97,32 @@ public class HuffmanCompressor {
         return returnList;
     }
 
-    public void compress(String file) throws Exception{
+    public void compress(String inputFile, String outputFile) throws Exception{
         FileInputStream fileInput;
         DataInputStream dataInput;
         byte[] byteData;
         ArrayList<Character> charList= new ArrayList<>();
         try{
-            fileInput = new FileInputStream(file);
+            fileInput = new FileInputStream(inputFile);
             dataInput = new DataInputStream(new BufferedInputStream(fileInput));
 
         }catch (FileNotFoundException e){
             throw new Exception("File not found!\nError message:\n" + e);
         }
 
+        DataOutputStream dataOutput = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(outputFile)));
+
         byteData = new byte[fileInput.available()];
 
         dataInput.read(byteData);
 
+        dataInput.close();
+
         ArrayList<Node> treeList = findFrequency(byteData);
 
         Node[] leafNodeArray = treeList.toArray(new Node[treeList.size()]);
+
+        Node root;
 
         while (treeList.size() > 1){
 
@@ -140,13 +142,92 @@ public class HuffmanCompressor {
             treeList.add(localRoot);
 
             Collections.sort(treeList);
-            System.out.println(Arrays.toString(treeList.toArray()));
         }
+
+        root = treeList.get(0);
+
+        //Generer bitsekvenser
+
+        String[] bitSequenceArray = new String[leafNodeArray.length];
+
+        Node currentNode;
+        Node localRoot;
+
+        for (int i = 0; i < bitSequenceArray.length; i++) {
+            currentNode = leafNodeArray[i];
+            bitSequenceArray[i] = "";
+
+            while (currentNode.getRoot() != null){
+                localRoot = currentNode.getRoot();
+
+                if (localRoot.getLeft() == currentNode){
+                    bitSequenceArray[i] = "0" + bitSequenceArray[i];
+                    System.out.println("Generated 0");
+                }else{
+                    bitSequenceArray[i] = "1" + bitSequenceArray[i];
+                    System.out.println("Generated 1");
+                }
+                currentNode = localRoot;
+            }
+        }
+
+        for (int i = 0; i < bitSequenceArray.length; i++) {
+            System.out.print((char) leafNodeArray[i].data);
+            System.out.println(bitSequenceArray[i]);
+        }
+
+
+        //Generer bitString
+
+        String bitString = "";
+        StringBuilder sb = new StringBuilder();
+
+        byte currentByte;
+
+        for (int i = 0; i < byteData.length; i++) {
+            currentByte = byteData[i];
+
+            //find compressed value for current byte
+            for (int j = 0; j < leafNodeArray.length; j++) {
+                if (currentByte == leafNodeArray[j].getData()){
+                    sb.append(bitSequenceArray[j]);
+                    break;
+                }
+            }
+        }
+
+        bitString = sb.toString();
+
+        //Konverter tekst
+
+        BitSet bitSet = new BitSet();
+
+        System.out.println("bitstring length: " + bitString.length());
+
+        for (int i = 0; i < bitString.length(); i++) {
+            if (bitString.charAt(i) == '0'){
+                bitSet.set(i, false);
+            }else {
+                bitSet.set(i, true);
+            }
+        }
+
+        // Export
+        byte[] exportByteArray = bitSet.toByteArray();
+        bitSet.
+        for (byte b: exportByteArray) {
+            dataOutput.writeByte(b);
+            System.out.println("Byte written: " + b);
+        }
+
+        dataOutput.close();
+
+        System.out.println("Done!");
+
     }
 
     public static void main(String[] args) throws Exception{
         HuffmanCompressor huffmanCompressor = new HuffmanCompressor();
-        huffmanCompressor.compress("src/opg12.txt");
+        huffmanCompressor.compress("src/opg12.txt", "src/output.txt");
     }
 }
-
